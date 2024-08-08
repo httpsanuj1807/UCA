b4e759fc2cfff48beecc89bfc9000b5aa43fc953
root <root@myvm19>
2024-08-08 11:10:27 +0000

    Added essential programs

---
+++ utility-programs/calc-time.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    
+    clock_t start_time, end_time;
+    double cpu_time_used;
+
+    // Record the start time
+    start_time = clock();
+
+    // Place your code to measure execution time here
+    int sum = 0;
+
+    for(int i=1 ; i<=1000000 ; i++){
+        sum+=i;
+    }
+
+    // Record the end time
+    end_time = clock();
+
+    // Calculate the CPU time used in seconds
+    cpu_time_used = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
+
+    printf("Sum: %d\n", sum);
+    printf("CPU Time Used: %f seconds\n", cpu_time_used);
+
+    return 0;
+}
+
+
+
+
